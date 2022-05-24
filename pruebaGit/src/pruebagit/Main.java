f2e0d460094e91fb274fa5e1502138f086c72b4e
Soto0799 <98480980+Soto0799@users.noreply.github.com>
2022-05-24 00:56:05 -0600

    Herencia

--- pruebaGit/src/pruebagit/Main.java
+++ pruebaGit/src/pruebagit/Main.java
@@ -1,13 +1,34 @@
 package pruebagit;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
     public static void main(String[] args) {
-        
-        System.out.println("Hola mundo virtual");
-        System.out.println("Probando lo segundo");
-        System.out.println("Programando en Git");
-        System.out.println("Terminando");
+
+        Profesor profeUno = new Profesor();
+        Profesor profeDos = new Profesor();
+        Estudiante estudiando = new Estudiante();
+
+        profeUno.setNombre(JOptionPane.showInputDialog("Digite el nombre del profesor : "));
+        profeUno.setApellido(JOptionPane.showInputDialog("Favor digite el primer apellido del profesor : "));
+        profeUno.setEdad(Integer.parseInt(JOptionPane.showInputDialog("Digite la edad del profesor : ")));
+        profeUno.setProvincia(JOptionPane.showInputDialog("Digite el lugar de residencia del profesor : "));
+
+        profeDos.setNombre(JOptionPane.showInputDialog("Digite el nombre del profesor : "));
+        profeDos.setApellido(JOptionPane.showInputDialog("Favor digite el primer apellido del profesor : "));
+        profeDos.setEdad(Integer.parseInt(JOptionPane.showInputDialog("Digite la edad del profesor : ")));
+        profeDos.setProvincia(JOptionPane.showInputDialog("Digite el lugar de residencia del profesor : "));
+
+        estudiando.setNombre(JOptionPane.showInputDialog("Digite el nombre del estudiante : "));
+        estudiando.setApellido(JOptionPane.showInputDialog("Digite el apellido del estudiante: "));
+        estudiando.setEdad(Integer.parseInt(JOptionPane.showInputDialog("Digite la edad del estudiante : ")));
+        estudiando.setProvincia(JOptionPane.showInputDialog("Digite donde vive el estudiante: "));
+
+        JOptionPane.showMessageDialog(null,
+                "El profesor #1 se llama " + profeUno.getNombre() + " " + profeUno.getApellido() + " tiene una edad de " + profeUno.getEdad() + " y vive en " + profeUno.getProvincia() + "\n\n"
+                + "El profesor #2 se llama " + profeUno.getNombre() + " " + profeUno.getApellido() + " tiene una edad de " + profeUno.getEdad() + " y vive en " + profeUno.getProvincia() + "\n\n"
+                + "El estudiante seleccionado se llama : "+estudiando.getNombre()+" "+estudiando.getApellido()+" teniendo una edad de "+estudiando.getEdad()+" y vive en la provincia de "+estudiando.getProvincia());
 
     }
 
---
+++ pruebaGit/src/pruebagit/Profesor.java
@@ -0,0 +1,42 @@
+package pruebagit;
+
+public class Profesor extends Estudiante{
+    
+    public Profesor(){
+        super();
+    }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+      
+}
