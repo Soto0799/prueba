1a3a82e96262a1c46269e45f89ca33f347aaea1e
Soto0799 <98480980+Soto0799@users.noreply.github.com>
2022-05-23 23:59:53 -0600

    Desde Git

--- pruebaGit/src/pruebagit/Main.java
+++ pruebaGit/src/pruebagit/Main.java
@@ -6,6 +6,7 @@ public class Main {
         
         System.out.println("Hola mundo virtual");
         System.out.println("Probando lo segundo");
+        System.out.println("Programando en Git");
 
     }
 
